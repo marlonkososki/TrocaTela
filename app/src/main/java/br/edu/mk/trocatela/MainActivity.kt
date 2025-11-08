a33a760e22e89219541ee89f754256407f01ab14
Marlon Kososki <marlon.kososki@gmail.com>
2025-11-08 13:55:52 -0300

    Criação e chamada da tela Lançamento.

--- app/src/main/java/br/edu/mk/trocatela/MainActivity.kt
+++ app/src/main/java/br/edu/mk/trocatela/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.edu.mk.trocatela
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -10,16 +11,15 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 
-    fun btLancamentoOnClick(view: View) {}
+    fun btLancamentoOnClick(view: View) {
+
+        val intent = Intent(this, LancamentoActivity::class.java)
+
+
+    }
 
 
 }
